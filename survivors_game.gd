eeb17dffb698698b8db9357b94308d02e1edca3c
Noah-C-R <nore7537@colorado.edu>
2025-09-09 20:41:37 -0600

    Resolved Issues with Pausing
    Processing issues were resolved so the game can pause and reload properly

--- survivors_game.gd
+++ survivors_game.gd
@@ -1,5 +1,8 @@
 extends Node2D
 
+func _ready() -> void:
+	get_tree().paused = false
+
 func spawn_enemy():
 	var new_enemy = preload("res://enemy.tscn").instantiate() #despite the syntax Godot knows to only call this once
 	%PathFollow2D.progress_ratio = randf()
@@ -16,6 +19,7 @@ func _on_player_health_depleted() -> void:
 	%GameOverScreen.show_final_score()
 	
 	%ScoreScreen.visible = false
+	get_tree().paused = true
 
 func _on_button_pressed() -> void: #used to reload scene
 	get_tree().reload_current_scene()
