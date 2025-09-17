b0f619b9c7045855f1acd9b320fd7b639302d644
Noah-C-R <nore7537@colorado.edu>
2025-09-16 20:53:42 -0600

    Gems Visual Update Fix
    A dirty way of ensuring the gems visual stays updated by refreshing it in _processing

--- scenes/gems_screen.gd
+++ scenes/gems_screen.gd
@@ -6,7 +6,5 @@ func _ready():
 func update_label():
 	%GemsLabel.text = ("x" + str(StatsHolder.gems))
 
-
-func _on_stats_holder_gems_changed() -> void:
-	print("we got the signal from stats_holder")
+func _process(delta: float) -> void:
 	update_label()
