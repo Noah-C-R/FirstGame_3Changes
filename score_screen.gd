44a4f9730706c6c979ad3706ba0d8045c61b76b7
Noah-C-R <nore7537@colorado.edu>
2025-09-16 22:26:54 -0600

    Level Up Thresholds
    The game now requires more kills to keep leveling up

--- score_screen.gd
+++ score_screen.gd
@@ -1,13 +1,15 @@
 extends CanvasLayer
 
 var score := 0
-var level_up_interval := 5
+var level_up_index := 0
+
 signal level_up
 
 func add_points():
 	score += 1
 	%ScoreLabel.text = str(score)
 	
-	if score % level_up_interval == 0:
-		StatsHolder.level += 1
+	if score == StatsHolder.level_thresholds[clamp(StatsHolder.level, 0, StatsHolder.level_thresholds.size() -1)]:
+		if StatsHolder.level < StatsHolder.level_thresholds.size():
+			StatsHolder.level += 1
 		level_up.emit()
--- stats_holder.gd
+++ stats_holder.gd
@@ -3,7 +3,9 @@ extends Node
 signal gems_changed
 
 var level := 0
-var gems := 10
+var gems := 0
+
+var level_thresholds = [5, 10, 25, 45, 60, 90, 150]
 
 var stat_options = {"Health": [100, 5], "Movement": [600, 5],  "Firing": [.7, 5]} #[value, cost]
 
@@ -12,4 +14,3 @@ func update_gems(cost):
 	print("Gems held is now: ", gems)
 	
 	gems_changed.emit()
-	
