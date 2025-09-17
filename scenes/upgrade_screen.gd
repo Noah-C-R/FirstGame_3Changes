552dc1a6f569a12e20de7a2972f8ffe01ee77f04
Noah-C-R <nore7537@colorado.edu>
2025-09-16 21:40:20 -0600

    Upgrade Screen Pauses
    Upgrade screen pauses the game

--- scenes/upgrade_screen.gd
+++ scenes/upgrade_screen.gd
@@ -2,9 +2,11 @@ extends CanvasLayer
 
 func _on_score_screen_level_up() -> void:
 	self.visible = true
+	get_tree().paused = true
 	#pause the game -- may need to put menus under a separate node which does not get affected by pausing
 
 
 func _on_exit_button_pressed() -> void:
 	self.visible = false
+	get_tree().paused = false
 	#unpause the game
